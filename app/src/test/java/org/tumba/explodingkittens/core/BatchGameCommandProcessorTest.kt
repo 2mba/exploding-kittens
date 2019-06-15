d572b5396ee9614d96a702051d14eb138394a82e
obairka <obairka@gmail.com>
2019-06-15 23:36:19 +0700

    add tests to BatchGameCommandProcessorTest

---
+++ app/src/test/java/org/tumba/explodingkittens/core/BatchGameCommandProcessorTest.kt
@@ -0,0 +1,50 @@
+package org.tumba.explodingkittens.core
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+class BatchGameCommandProcessorTest {
+
+    private lateinit var gameState: GameState
+    private lateinit var gameManager: GameManager
+    private lateinit var commands: List<GameCommand>
+    private lateinit var processors: List<GameCommandProcessor>
+    private lateinit var processor: BatchGameCommandProcessor
+
+    @Before
+    fun setup() {
+        gameState = mockk()
+        gameManager = mockk()
+        commands = listOf(mockk(), mockk())
+        commands.forEachIndexed { index, command ->
+            every { command.id } returns index.toString()
+        }
+
+        processors = commands.map { command ->
+            val proccessor = mockk<GameCommandProcessor>(relaxed = true)
+            every { proccessor.commandId } answers { command.id }
+            proccessor
+        }
+        processor = BatchGameCommandProcessor(processors)
+    }
+
+    @Test
+    fun `process should call process only on matched commands`() {
+        processor.process(commands[0], gameState, gameManager)
+
+        verify {
+            processors[0].process(commands[0], gameState, gameManager)
+        }
+
+        verify(exactly = 0) {
+            processors[0].process(neq(commands[0]), any(), any())
+        }
+
+        verify(exactly = 0) {
+            processors[1].process(any(), any(), any())
+        }
+    }
+}
